dc64c0fecd2507d8ec1ccb3c4c2cf167c15c991b
WFkSky <moises.mac@gmail.com>
2021-04-22 13:19:57 -0500

    Actualizar operacion Multiplicacion

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,16 @@ class Calculator{
 	    int c=a+b;	
 	    return c; 
     } 
+    
     int sub(int a, int b){ return 0; }
-    int mul(int a, int b){ return 0; }
+    
+    int mul(int a, int b){
+        int mul = a * b;
+        return mul;
+    }
+    
     int div(int a, int b){ return 0; }
+    
     int mod(int a, int b){ return 0; }
 
 }
